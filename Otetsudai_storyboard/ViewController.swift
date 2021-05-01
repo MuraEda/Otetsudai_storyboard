d83fabbf1413be0a883e5621d467b7f3b3bfe0ee
yusuke <44flkpdx@gmail.com>
2021-05-01 11:57:39 +0900

    Initial Commit

---
+++ Otetsudai_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Otetsudai_storyboard
+//
+//  Created by Murase Yusuke on 2021/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
